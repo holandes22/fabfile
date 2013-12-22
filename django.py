7f2f4bbd3720046228d6f09f9bfcde39d92bc932
Pablo Klijnjan <pablokl@il.ibm.com>
2013-12-22 09:25:32 +0200

    Using fabric tasks

--- django.py
+++ django.py
@@ -1,59 +1,99 @@
+from fabric.api import task
 from fabric.api import env, run, require
 from fabric.operations import prompt
 from common import virtualenv
 
 
-def _verify_migration_app_is_set():
+def verify_migration_app_is_set():
     if not 'schemamigration_app' in env:
-        env.schemamigration_app = prompt('App name for Schema migration:', default='')
+        env.schemamigration_app = prompt(
+            'App name for Schema migration:',
+            default='',
+        )
 
 
-def _verify_migration_initial_is_set():
+def verify_migration_initial_is_set():
     if not 'schemamigration_initial' in env:
         env.schemamigration_initial = prompt('Initial? [y|n]:', default='n')
 
 
+@task
 def migrate():
     # if app == '', south will look for all migrations in all apps
-    _verify_migration_app_is_set()
+    verify_migration_app_is_set()
     with virtualenv():
-        run('python manage.py migrate {} --settings={}'.format(env.schemamigration_app, env.settings_file))
+        run(
+            'python manage.py migrate {} --settings={}'.format(
+                env.schemamigration_app,
+                env.settings_file,
+            )
+        )
 
 
+@task
 def schemamigration():
     initial = ''
-    _verify_migration_app_is_set()
-    _verify_migration_initial_is_set()
+    verify_migration_app_is_set()
+    verify_migration_initial_is_set()
     app = env.schemamigration_app
     initial = '--initial' if env.schemamigration_initial == 'y' else '--auto'
     with virtualenv():
-        run('python manage.py schemamigration {initial} {app} --settings={settings}'.format(initial=initial,
-                                                                                            app=app, settings=env.settings_file))
+        run(
+            'python manage.py schemamigration {} {} --settings={}'.format(
+                initial,
+                app,
+                env.settings_file
+            )
+        )
 
 
+@task
 def django_shell():
     with virtualenv():
         run('python manage.py shell --settings={}'.format(env.settings_file))
 
 
+@task
 def collectstatic():
     with virtualenv():
-        run('python manage.py collectstatic --settings={}'.format(env.settings_file))
+        run(
+            'python manage.py collectstatic --settings={}'.format(
+                env.settings_file
+            )
+        )
 
+
+@task
 def collectstatic_aws():
     require('aws_access_key_id', 'aws_secret_access_key')
     collectstatic()
 
+
+@task
 def syncdb():
     with virtualenv():
-        run('python manage.py syncdb --noinput --settings={}'.format(env.settings_file))
+        run(
+            'python manage.py syncdb --noinput --settings={}'.format(
+                env.settings_file
+            )
+        )
 
 
+@task
 def createsuperuser():
     with virtualenv():
-        run('python manage.py createsuperuser --settings={}'.format(env.settings_file))
+        run(
+            'python manage.py createsuperuser --settings={}'.format(
+                env.settings_file
+            )
+        )
 
 
+@task
 def runserver():
     with virtualenv():
-        run('python manage.py runserver [::]:8000 --settings={}'.format(env.settings_file))
+        run(
+            'python manage.py runserver [::]:8000 --settings={}'.format(
+                env.settings_file
+            )
+        )
--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,19 @@
-from fabric.api import env, run
+from fabric.api import env, run, task
 from common import virtualenv
 
 
 PID_PATH = '/tmp/gunicorn.pid'
 
+
+@task
 def stopwsgi():
     run('kill `cat {}`'.format(PID_PATH))
 
 
+@task
 def startwsgi():
     with virtualenv():
-        args = '-w 9 -k gevent --max-requests 250 --preload --pid={}'.format(PID_PATH)
+        args = '-w 9 -k gevent --max-requests 250 --preload --pid={}'.format(
+            PID_PATH
+        )
         run('python manage.py run_gunicorn 0.0.0.0:8000 --settings={} {}'.format(env.settings_file, args))
