1383f0752bafd09175e16e9f3bcac8b615464d98
Pablo Klijnjan <pabloklijnjan@gmail.com>
2014-09-14 14:29:52 +0300

    Running supervisor commands as root

--- supervisor.py
+++ supervisor.py
@@ -1,15 +1,15 @@
 from fabric.api import task
-from fabric.api import env, run
+from fabric.api import env, sudo
 from common import virtualenv
 
 
 @task
 def restart_celery():
     with virtualenv():
-        run('{}/bin/supervisorctl restart celery'.format(env.virtualenv))
+        sudo('{}/bin/supervisorctl restart celery'.format(env.virtualenv))
 
 
 @task
 def restart_celerybeat():
     with virtualenv():
-        run('{}/bin/supervisorctl restart celerybeat'.format(env.virtualenv))
+        sudo('{}/bin/supervisorctl restart celerybeat'.format(env.virtualenv))
